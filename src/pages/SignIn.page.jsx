98c3f1ac023473ada3a0b6f5019d056dd77b5184
Pedro Nascimento <sn.jaypee@gmail.com>
2023-08-16 15:38:38 -0300

    Revert "add signIN and signUP on api service"
    This reverts commit 0357e3dd5804f8511e728c50c6d17f0016d46039.

--- src/pages/SignIn.page.jsx
+++ src/pages/SignIn.page.jsx
@@ -1,52 +1,14 @@
-import React, { useRef } from "react"
+import React from "react"
 import LinkrTitle from "../components/LinkrTitle.component"
 import { styled } from "styled-components"
-import axios from "axios";
 
 function SignIn() {
-  
-  const username = useRef();
-  const profileUrl = useRef();
-  const email = useRef();
-  const password = useRef();
-  
-  function submit () {
-
-    const data = {
-      email: email.current.value,
-      password: password.current.value,
-      username: username.current.value,
-      profileUrl: profileUrl.current.value
-    };
-    
-    axios.get()
-
-  }
-
-
   return (
     <SignInContainer>
       <LinkrTitle />
-        <FormContainer>
-            <form onClick={submit}>
-              <label htmlFor="email">email</label>
-              <input name="email" type="email" placeholder="e-mail" ref={email} />
-    
-              <label htmlFor="password">password</label>
-              <input name="password" type="password" placeholder="password" ref={password} />
-
-              <label htmlFor="username">username</label>
-              <input name="username" type="text" placeholder="username" ref={username} /> 
-    
-              <label htmlFor="profileUrl">picture url</label>
-              <input name="profileUrl" type="text" placeholder="picture url" ref={profileUrl} />
-    
-              <button type="submit"/>
-            </form>
-        </FormContainer>
+      <FormContainer></FormContainer>
     </SignInContainer>
   )
-
 }
 
 const FormContainer = styled.div`
