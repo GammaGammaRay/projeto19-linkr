038a558a654db74c4d002f767aecee8c62bb1fff
Wesley da Silva Ribeiro <wesleysilvaribeiro9@gmail.com>
2023-08-21 17:30:12 -0300

    [FIX] fixed api url on all files

--- src/components/HashtagPostList.component.jsx
+++ src/components/HashtagPostList.component.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 function HashtagPosts({tagName}) {
   
 
-  const API_URL = process.env.API_URL || "http://localhost:5000";
+  const API_URL = process.env.REACT_APP_API_URL;
 
   const { token } = useAuth();
   const config = { headers: { Authorization: `Bearer ${token}` } };
